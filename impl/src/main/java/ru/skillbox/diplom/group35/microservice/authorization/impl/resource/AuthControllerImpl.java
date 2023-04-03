e3bd3e8de6d60f9c33a0eff41ddcccc36647116d
Михаил Чечёткин <9859052640@mail.ru>
2023-04-03 13:07:23 +0300

    captcha validation
    captcha test
    
    captcha test
    
    captcha test
    
    captcha test
    
    captcha test
    
    captcha test
    
    captcha validation
    
    cancel captcha validation
    
    captcha validation
    
    remove captcha validation
    
    font
    
    changelog
    
    Update .gitlab-ci.yml file
    
    changelog
    
    changelog
    
    changelog
    
    added yml and dockerfile
    
    Update .gitlab-ci.yml file

--- impl/src/main/java/ru/skillbox/diplom/group35/microservice/authorization/impl/resource/AuthControllerImpl.java
+++ impl/src/main/java/ru/skillbox/diplom/group35/microservice/authorization/impl/resource/AuthControllerImpl.java
@@ -62,6 +62,7 @@ public class AuthControllerImpl implements AuthController {
 
     @Override
     public ResponseEntity<CaptchaDto> captcha() {
+        log.info("Getting captcha");
         return ResponseEntity.ok(captchaService.getCaptcha());
     }
 }
